cfb5f6967b46b8790c4f8e8702a42de8de3ee8a3
tassadit-ouzia <tassadit.ouzia04@gmail.com>
2024-05-18 22:52:58 +0200

    version de TassaditOuzia

--- Multiplication.java
+++ Multiplication.java
@@ -1,25 +1,14 @@
 public class Multiplication extends Operation{
-    private Nombre operande1;
-    private Nombre operande2;
 
-    public Multiplication(Nombre operandeNum1, Nombre operandeNum2){
-        this.operande1 = operandeNum1; 
-        this.operande2 = operandeNum2; 
+    public Multiplication(Nombre operande1, Nombre operande2){
+       super(operande1, operande2);
     }
 
     public int valeur(){
-        return operande1.valeur() * operande2.valeur();
-    }
-
-    public Nombre getOperande1() {
-        return operande1;
-    }
-
-    public Nombre getOperande2() {
-        return operande2;
+        return getOperande1().valeur() * getOperande2().valeur();
     }
 
     public String toString() {
-        return operande1 + " * " + operande2 + "="+ valeur();
+        return "(" + getOperande1() + " * " + getOperande2() + ")";
     }
 }
--- Soustraction.java
+++ Soustraction.java
@@ -1,25 +1,15 @@
 public class Soustraction extends Operation{
-    private Nombre operande1;
-    private Nombre operande2;
 
-    public Soustraction(Nombre operandeNum1, Nombre operandeNum2){
-        this.operande1 = operandeNum1; 
-        this.operande2 = operandeNum2; 
+    public Soustraction(Nombre operande1, Nombre operande2){
+       super(operande1, operande2);
     }
 
-    public int valeur(Nombre operande1, Nombre operande2){
-        return operande1.valeur() - operande2.valeur();
+    public int valeur(){
+        return getOperande1().valeur() - getOperande2().valeur();
     }
 
-    public Nombre getOperande1() {
-        return operande1;
-    }
-
-    public Nombre getOperande2() {
-        return operande2;
-    }
 
     public String toString() {
-        return operande1 + " - " + operande2 + "="+ this.valeur();
+        return "(" + getOperande1() + " - " + getOperande2() + ")";
     }
 }
