210a8cb84416bdd797c37548b138439ebfbb9409
tassadit-ouzia <tassadit.ouzia04@gmail.com>
2024-05-18 23:37:23 +0200

    version de TassditOuzia

--- Opération.java
+++ Opération.java
@@ -1,20 +1,19 @@
-public abstract class  Operation{
+public abstract class Operation {
     private Nombre operande1;
     private Nombre operande2;
 
-public Operation(Nombre operande1, Nombre operande2) {
+    public Operation(Nombre operande1, Nombre operande2) {
         this.operande1 = operande1;
         this.operande2 = operande2;
     }
 
     public abstract int valeur();
 
-    public abstract Nombre getOperande1(){
+    public Nombre getOperande1() {
         return this.operande1;
     }
 
-    public abstract Nombre getOperande2(){
-        return this.operande2
+    public Nombre getOperande2() {
+        return this.operande2;
     }
-
 }
